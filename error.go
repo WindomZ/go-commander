475ddc8fb8e65c22fcfae43d7fc2f8c14bf55b04
WindomZ <zwdsdtc@gmail.com>
2017-02-28 18:28:03 +0800

    feat: new error function

--- error.go
+++ error.go
@@ -3,6 +3,9 @@ package commander
 import "errors"
 
 var (
-	ErrNil    error = errors.New("error nil")
-	ErrOption       = errors.New("error option")
+	ErrNil     error = errors.New("error nil")
+	ErrCommand       = errors.New("error command")
+	ErrOption        = errors.New("error option")
 )
+
+type ErrFunc func(err error, obj interface{})
