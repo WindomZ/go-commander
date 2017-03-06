3aa0def12257c7ce1f2fd0558380596e3f2310d9
WindomZ <zwdsdtc@gmail.com>
2017-03-06 12:33:46 +0800

    feat: options impl & test

--- options.go
+++ options.go
@@ -1,7 +1,25 @@
 package commander
 
+import "fmt"
+
 type Options []*Option
 
-func (o Options) OptionsString() []string {
-	return []string{}
+func (o Options) UsagesString() (r []string) {
+	for _, opt := range o {
+		if opt.IsRequired() {
+			r = append(r, fmt.Sprintf("(%s)",
+				opt.UsageString()))
+		} else {
+			r = append(r, fmt.Sprintf("[%s]",
+				opt.UsageString()))
+		}
+	}
+	return
+}
+
+func (o Options) OptionsString() (r []string) {
+	for _, opt := range o {
+		r = append(r, opt.OptionString())
+	}
+	return
 }
---
+++ options_test.go
@@ -0,0 +1,38 @@
+package commander
+
+import (
+	"github.com/WindomZ/testify/assert"
+	"testing"
+)
+
+func TestOptions_UsagesString(t *testing.T) {
+	o := Options{
+		newOption("-a, --about", "about description"),
+		newOption("-b, --bold=<kn>", "bold description"),
+		newOption("-c, --config", "config description"),
+		newOption("-d, --drop", "drop description"),
+	}
+	assert.Equal(t, o.UsagesString(),
+		[]string{
+			"[-a|--about]",
+			"[(-b|--bold)=<kn>]",
+			"[-c|--config]",
+			"[-d|--drop]",
+		})
+}
+
+func TestOptions_OptionsString(t *testing.T) {
+	o := Options{
+		newOption("-a, --about", "about description"),
+		newOption("-b, --bold=<kn>", "bold description"),
+		newOption("-c, --config", "config description"),
+		newOption("-d, --drop", "drop description"),
+	}
+	assert.Equal(t, o.OptionsString(),
+		[]string{
+			"-a, --about   about description",
+			"-b, --bold    bold description",
+			"-c, --config  config description",
+			"-d, --drop    drop description",
+		})
+}
