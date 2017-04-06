8d9c2b76016b6341f41544da73b4d2c571a72306
WindomZ <git.windomz@gmail.com>
2017-04-06 13:48:04 +0800

    test: program

--- program_test.go
+++ program_test.go
@@ -20,6 +20,34 @@ func TestProgram_AutomaticHelp(t *testing.T) {
 	}
 }
 
+func TestProgram_ShowVersion(t *testing.T) {
+	Program = newProgram()
+
+	Program.Version("0.0.1").
+		Description("this is a test cli.")
+
+	if _, err := Program.Parse([]string{"go-commander", "-v"}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProgram_ErrorHandling(t *testing.T) {
+	Program = newProgram()
+
+	Program.Version("0.0.1").
+		Description("this is a test cli.").
+		ErrorHandling(func(err error) {
+			assert.Error(t, err)
+		})
+	Program.Command("test").Action(func() error {
+		return newError("this is a test error")
+	})
+
+	if _, err := Program.Parse([]string{"go-commander", "test"}); err == nil {
+		assert.Error(t, err)
+	}
+}
+
 func TestProgram_LineOption(t *testing.T) {
 	var result string
 	var result2 string
